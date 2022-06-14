85fd3f43b054560a7b08835f8784b0337b24aba8
IgorTenoso <lucasmirandaneves@gmail.com>
2022-06-14 18:40:36 -0300

    mensagem alteradas, agora com o valor do disjuntor necessário

--- provider/Secao/index.js
+++ provider/Secao/index.js
@@ -14,25 +14,25 @@ export const SecaoProvider = ({ children }) => {
 
     else if(type === "corrente"){
       if (value >= 1 & value < 9){
-        return setSecao("A seção é: 0.5mm²")
+        return setSecao("A seção é: 0.5mm², porém a seção mínima admissível é 1,5 mm²")
       }
       else if (value >= 1 & value < 11){
-        return setSecao("A seção é: 0.75mm²")
+        return setSecao("A seção é: 0.75mm², porém a seção mínima admissível é 1,5 mm²")
       }
       else if (value >= 1 & value < 14){
-        return setSecao("A seção é: 1mm²")
+        return setSecao("A seção é: 1mm², porém a seção mínima admissível é 1,5 mm²")
       }
       else if (value >= 1 & value < 17.5){
-        return setSecao("A seção é: 1.5mm²")
+        return setSecao("A seção é: 1.5mm². E a corrente nominal do disjuntor é: 10A ")
       }
       else if (value >= 1 & value < 24){
-        return setSecao("A seção é: 2.5mm²")
+        return setSecao("A seção é: 2.5mm². E a corrente nominal do disjuntor é: 15A")
       }
       else if (value >= 1 & value < 32){
-        return setSecao("A seção é: 4mm²")
+        return setSecao("A seção é: 4mm². E a corrente nominal do disjuntor é: 20A")
       }
       else if (value >= 1 & value <= 41){
-        return setSecao("A seção é: 6mm²")
+        return setSecao("A seção é: 6mm². E a corrente nominal do disjuntor é: 25A")
       }
       else if (value < 1 || value > 41){
         return setSecao("Esse valor não é destinado á residências")
@@ -41,25 +41,25 @@ export const SecaoProvider = ({ children }) => {
 
     else{
       if (value >= 1 & value < 1143){
-        return setSecao("A seção é: 0.5mm²")
+        return setSecao("A seção é: 0.5mm², porém a seção mínima admissível é 1,5 mm²")
       }
       else if (value >= 1 & value < 1397){
-        return setSecao("A seção é: 0.75mm²")
+        return setSecao("A seção é: 0.75mm², porém a seção mínima admissível é 1,5 mm²")
       }
       else if (value >= 1 & value < 1778){
-        return setSecao("A seção é: 1.0mm²")
+        return setSecao("A seção é: 1mm², porém a seção mínima admissível é 1,5 mm²")
       }
       else if (value >= 1 & value < 2222.5){
-        return setSecao("A seção é: 1.5mm²")
+        return setSecao("A seção é: 1.5mm². E a corrente nominal do disjuntor é: 10A ")
       }
       else if (value >= 1 & value < 3048){
-        return setSecao("A seção é: 2.5mm²")
+        return setSecao("A seção é: 2.5mm². E a corrente nominal do disjuntor é: 15A")
       }
       else if (value >= 1 & value < 4064){
-        return setSecao("A seção é: 4mm²")
+        return setSecao("A seção é: 4mm². E a corrente nominal do disjuntor é: 20A")
       }
       else if (value >= 1 & value <= 5207){
-        return setSecao("A seção é: 6mm²")
+        return setSecao("A seção é: 6mm². E a corrente nominal do disjuntor é: 25A")
       }
       else if (value <= 1 || value > 5207){
         return setSecao("Esse valor não é destinado á residências")
